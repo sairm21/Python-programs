0911b6488ea19be46774ef3e095e5c57911cb5df
sairm21 <89889572+sairm21@users.noreply.github.com>
2025-08-02 12:16:47 +0530

    Grade checker program

---
+++ grade_checker.py
@@ -0,0 +1,12 @@
+score=int(input("Enter your score: "))
+
+if score >= 90:
+    print("You have an A grade.")
+elif score >= 80:
+    print("You have a B grade.")
+elif score >= 70:
+    print("You have a C grade.")
+elif score >= 60:
+    print("You have a D grade.")
+else:
+    print("You have failed.")   
